a0719639572e65dc3884b2328b25841abb7d90c5
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-12 15:52:01 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,6 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
-sfdx force:source:push
-sfdx force:org:open
-sfdx force:user:permset:assign -n CustomObjectPerms
-sfdx automig:load -d data --concise
-sfdx shane:user:password:set -p sfdx1234 -g User -l User
+sf org create scratch -f config/project-scratch-def.json -d 2 -s
+sf project deploy start
+sf org open
+sf org assign permset -n CustomObjectPerms
+sf automig load -d data --concise
+sf demoutil user password set -p salesforce1 -g User -l User
